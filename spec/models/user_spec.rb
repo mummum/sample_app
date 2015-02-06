de8c9b664581974327f44c58ba4ea380a8502002
Karasu <flysam2@gmail.com>
2015-02-06 10:41:23 +0900

    6章完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,7 +36,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com user@cool..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -59,6 +59,16 @@ describe User do
       user_with_same_email.save
     end
     it { should_not be_valid }
+  end
+  
+  describe "email is conaverted downcase" do
+    let(:mixed_case_addr) { "fdsFFassF.@Vnf.com" }
+    
+    it "should be converted downcase" do
+      @user.email = mixed_case_addr
+      @user.save
+      expect(@user.reload.email).to eq  mixed_case_addr.downcase
+    end
   end
   
   describe "when password is not present" do
